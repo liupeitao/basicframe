3b20f6fb884f5c8f112853a7d8a05a1548021dcb
PTking <sudolovelnpctly6869@outlook.com>
2023-07-02 23:30:42 +0800

    init  基本项目结构。以及配置文件

---
+++ basicframe/playground/asyncdemo.py
@@ -0,0 +1,125 @@
+import asyncio
+import math
+import os
+import subprocess
+import time
+import redis
+
+# 控制并发数为100
+semaphore = asyncio.Semaphore(100)
+from basicframe.midware.redisclient import RedisClient
+redis_conn = RedisClient().connect()
+
+
+def generate_std_filename(website_name, duration, key_or_other):
+    filename = f'{website_name}_{duration}_' + key_or_other.replace('/', '_').replace(':', '_').replace('.',
+                                                                                                        '_') + '.mp4'
+    return filename
+
+
+async def get_m3u8_duration(m3u8_url):
+    async with semaphore:
+        print(m3u8_url)
+
+        # m3u8_url = 'https://v5.cdtlas.com/20220619/6hCH3rE1/index.m3u8'
+        command = ['ffprobe', '-v', 'error', '-show_entries', 'format=duration', '-of',
+                   'default=noprint_wrappers=1:nokey=1', '-i', m3u8_url]
+
+        process = await asyncio.create_subprocess_exec(*command, stdout=asyncio.subprocess.PIPE,
+                                                       stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await process.communicate()
+        print(process.returncode)
+        print(stdout.decode())
+        print(stderr.decode())
+
+
+async def download_m3u8_video(m3u8_url, path):
+    print('正在下载视频: ', m3u8_url)
+    try:
+        command = f'ffmpeg -y -i {m3u8_url} -c copy {path}'
+        process = await asyncio.create_subprocess_shell(command)
+        await process.wait()
+        print(f'Successfully downloaded M3U8 video and saved as {path}.')
+    except subprocess.CalledProcessError as e:
+        print(e)
+        raise Exception
+
+
+# async def task(url):
+#     m3u8_url = 'https://v5.cdtlas.com/20220619/6hCH3rE1/index.m3u8'
+#     print(url)
+#     await asyncio.sleep(2)
+
+#     # await download_m3u8(m3u8_url, os.path.join(dir, file_name))
+
+
+# async def main():
+#     for i in range(10):
+#         await task(i)
+
+# start = time.time()
+# asyncio.run(main())
+
+# end = time.time()
+# print(end - start)
+
+
+# def s():
+#     yield redis_conn.rpop('fwe')
+
+async def task(url):
+    m3u8_url = 'https://v5.cdtlas.com/20220619/6hCH3rE1/index.m3u8'
+    print(url)
+
+    # await download_m3u8(m3u8_url, os.path.join(dir, file_name))
+
+
+m_list = redis_conn.lrange('kmj_valid_m3u8', 0, 100)
+for m3u8 in m_list:
+    print(m3u8)
+
+
+async def main():
+    tasks = [get_m3u8_duration(i) for i in m_list]
+    # await asyncio.gather(*tasks)
+    await asyncio.wait(tasks)
+
+
+start = time.time()
+asyncio.run(main())
+end = time.time()
+print(end - start)
+
+# async def main():
+#     start = time.time()
+#     dir = '/home/liupeitao/src/Python/utils/'
+#     for i in range(5):
+#         try:
+#             m3u8_url = 'https://v5.cdtlas.com/20220619/6hCH3rE1/index.m3u8'
+#             await task(m3u8_url)
+#         except Exception as e:
+#             print('完成')
+#             break
+#     end = time.time()
+#     print(end - start)
+
+
+# redis_conn.rpop('kmj_valid_m3u8')
+# if __name__ == '__main__':
+#     dir = '/media/ptking/Elements/meiju/'
+#     while True:
+#         m3u8_url = redis_conn.rpop('kmj_valid_m3u8').decode()
+#         if not m3u8_url:
+#             break
+#         try:
+#             # 此处可以优化 异步
+#             duration = math.ceil(get_m3u8_duration(m3u8_url))
+#             file_name = generate_std_filename('91kmj', duration, m3u8_url)
+#             path = os.path.join(dir, file_name)
+#             if os.path.exists(path):
+#                 continue
+#             print("save to ", path)
+#             download_m3u8(m3u8_url, path)
+#         except Exception as e:
+#             redis_conn.lpush('not_down', m3u8_url)
+#             break
---
+++ basicframe/settings.py
@@ -0,0 +1,139 @@
+# Scrapy settings for basicframe project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://docs.scrapy.org/en/latest/topics/settings.html
+#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+import os
+
+BOT_NAME = "basicframe"
+
+SPIDER_MODULES = ["basicframe.spiders"]
+NEWSPIDER_MODULE = "basicframe.spiders"
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+# USER_AGENT = "basicframe (+http://www.yourdomain.com)"
+
+# Obey robots.txt rules
+ROBOTSTXT_OBEY = False
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
+CONCURRENT_REQUESTS = 16
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 3
+# The download delay setting will honor only one of:
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+# COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+# TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+# DEFAULT_REQUEST_HEADERS = {
+#    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+#    "Accept-Language": "en",
+# }
+
+# Enable or disable spider middlewares
+# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# SPIDER_MIDDLEWARES = {
+#    "basicframe.middlewares.BasicframeSpiderMiddleware": 543,
+# }
+
+# Enable or disable downloader middlewares
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
+DOWNLOADER_MIDDLEWARES = {
+    # "basicframe.middlewares.BasicframeDownloaderMiddleware": 543,
+    "basicframe.middlewares.ProxyMiddleware": 300
+}
+
+# Enable or disable extensions
+# See https://docs.scrapy.org/en/latest/topics/extensions.html
+# EXTENSIONS = {
+#    "scrapy.extensions.telnet.TelnetConsole": None,
+# }
+
+# Configure item pipelines
+# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+ITEM_PIPELINES = {
+    "basicframe.pipelines.BasicframePipeline": 301,
+}
+
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
+# AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+# AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+# AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+# AUTOTHROTTLE_DEBUG = False
+
+# Enable and configure HTTP caching (disabled by default)
+# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = "httpcache"
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = "scrapy.extensions.httpcache.FilesystemCacheStorage"
+
+# Set settings whose default value is deprecated to a future-proof value
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+FEED_EXPORT_ENCODING = "utf-8"
+
+
+# 解析yaml文件配置
+import yaml
+# 获取当前文件的目录路径
+current_dir = os.path.dirname(os.path.abspath(__file__))
+# 根据当前文件的目录路径拼接配置文件的路径
+config_file = os.path.join(current_dir, 'config.yaml')
+def load_config(file_path):
+    with open(file_path, 'r') as f:
+        config = yaml.safe_load(f)
+    return config
+
+
+# 导入配置
+config = load_config(config_file)
+
+# 获取Redis配置
+# 获取Redis配置
+REDIS_CONFIG = config['redis']
+REDIS_HOST = REDIS_CONFIG['host']
+REDIS_PORT = REDIS_CONFIG['port']
+REDIS_DB = REDIS_CONFIG['db']
+REDIS_PASSWORD = REDIS_CONFIG['password']
+
+# 获取MySQL配置
+MYSQL_CONFIG = config['mysql']
+MYSQL_HOST = MYSQL_CONFIG['host']
+MYSQL_PORT = MYSQL_CONFIG['port']
+MYSQL_DATABASE = MYSQL_CONFIG['database']
+MYSQL_USERNAME = MYSQL_CONFIG['username']
+MYSQL_PASSWORD = MYSQL_CONFIG['password']
+
+# 获取MongoDB配置
+MONGODB_CONFIG = config['mongodb']
+MONGODB_HOST = MONGODB_CONFIG['host']
+MONGODB_PORT = MONGODB_CONFIG['port']
+MONGODB_DATABASE = MONGODB_CONFIG['database']
+
+# 获取Celery配置
+CELERY_CONFIG = config['celery']
+CELERY_BROKER_URL = CELERY_CONFIG['broker_url']
+CELERY_RESULT_BACKEND = CELERY_CONFIG['result_backend']
+# 其他Celery配置参数...
+
